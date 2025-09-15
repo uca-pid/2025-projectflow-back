fe0b03674f248ea7b30fe6a79efe616c1ed95968
Santiago Papa <papa.santiago321@gmail.com>
2025-09-15 09:46:36 -0300

    So the google

--- auth.js
+++ auth.js
@@ -13,4 +13,11 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      enabled: true,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
 });
